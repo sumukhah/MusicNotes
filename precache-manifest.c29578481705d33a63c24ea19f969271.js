7b3aead63b70c5cf94ce437e143afc81f5ec8478
sumukhah <sumukha214@gmail.com>
2020-10-15 16:15:06 +0530

    Updates

---
+++ precache-manifest.c29578481705d33a63c24ea19f969271.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "016e01436ca342a6347a6b3863f66498",
+    "url": "/MusicNotes/index.html"
+  },
+  {
+    "revision": "576da47cc47b4b6d6db4",
+    "url": "/MusicNotes/static/css/2.6aee0b3d.chunk.css"
+  },
+  {
+    "revision": "659ab13b7acf102eca53",
+    "url": "/MusicNotes/static/css/main.65d08e2d.chunk.css"
+  },
+  {
+    "revision": "576da47cc47b4b6d6db4",
+    "url": "/MusicNotes/static/js/2.e464726a.chunk.js"
+  },
+  {
+    "revision": "f32d1c35a9b9c30a6bb9cb9b54f8cf0f",
+    "url": "/MusicNotes/static/js/2.e464726a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "659ab13b7acf102eca53",
+    "url": "/MusicNotes/static/js/main.f665a7b5.chunk.js"
+  },
+  {
+    "revision": "c10a6d6f351a74644c13",
+    "url": "/MusicNotes/static/js/runtime-main.6516aa20.js"
+  }
+]);
